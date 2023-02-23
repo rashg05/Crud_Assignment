3eaa13760c76d01bbe0a761a4fe76dce7f7f450d
rashg05 <gajra.rashmi0504@gmail.com>
2023-02-23 19:06:20 +0530

    multer updated and s3.js created

--- post_crud/src/routes/posts.routes.js
+++ post_crud/src/routes/posts.routes.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const multer = require("multer");
 const { postsController } = require("../controllers");
-// const commentsRoutes = require("./comments.routes");
+
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    
+    filename: function (req, file, cb) {
+        console.info(file.postImage, "--->");
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 
 const postsRoutes = express.Router({mergeParams: true});
@@ -11,6 +23,6 @@ postsRoutes.get('/:post_id', postsController.getPostById);
 postsRoutes.delete('/:post_id', postsController.deleteOnePost);
 postsRoutes.put('/:post_id', postsController.updateOnePost);
 // postsRoutes.use('/:post_id/comments', commentsRoutes);
-postsRoutes.post('/:post_id/cover', postsController.uploadPostCover);
+postsRoutes.post('/:post_id/cover', upload.single('postImage'), postsController.uploadPostCover);
 
 module.exports = postsRoutes;
