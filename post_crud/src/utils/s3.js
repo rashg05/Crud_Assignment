67bb14a5ad3e64869592f462061499a9711679dd
rashg05 <gajra.rashmi0504@gmail.com>
2023-02-28 13:41:56 +0530

    upload for s3

--- post_crud/src/utils/s3.js
+++ post_crud/src/utils/s3.js
@@ -1,10 +1,12 @@
 const { S3Client, UploadCommand } = require("@aws-sdk/client-s3");
-const { AWS_BUCKET_NAME, AWS_REGION } = require('../config');
+const { AWS_BUCKET, AWS_REGION } = require('../config');
+
+// const aws = require("aws-sdk");
 
 const uploadToS3 = async ({ body, key }) => {
   try {
     const params = {
-      Bucket: AWS_BUCKET_NAME,
+      Bucket: AWS_BUCKET,
       Key: key,
       Body: body,
     };
